469d8fbcd1b52b27c0de9e6d15c1653919a27b4d
이재백 <dbswlrla112@naver.com>
2024-11-25 17:55:25 +0900

    refactor: change back api domain

--- src/api/http-commons.js
+++ src/api/http-commons.js
@@ -5,7 +5,7 @@ const localAxios = () => {
     baseURL:
       process.env.NODE_ENV === "development"
         ? "http://localhost:8080/api/v1/"
-        : "https://ezip.world/api/v1/",
+        : "http://api.ezip.world/api/v1/",
     withCredentials: true,
     headers: {
       "Content-Type": "application/json;charset=utf-8",
