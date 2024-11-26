c992559be74197ebc25b8f70718260369ad13f07
이재백 <dbswlrla112@naver.com>
2024-11-27 00:56:24 +0900

    refactor: acces control allow origin

--- src/api/http-commons.js
+++ src/api/http-commons.js
@@ -6,6 +6,8 @@ const localAxios = () => {
     withCredentials: true,
     headers: {
       "Content-Type": "application/json;charset=utf-8",
+      "Access-Control-Allow-Origin": "https://ezip-lb-673574854.ap-northeast-2.elb.amazonaws.com/",
+      crossDomain: true,
     },
   });
   return instance;
